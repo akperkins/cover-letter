209031b96dacb080330308ba2aa2694317dc18b5
Andre Perkins <akperkins1@gmail.com>
2013-12-03 16:42:16 -0500

    refactoringcode

--- createCoverLetter.py
+++ createCoverLetter.py
@@ -2,31 +2,51 @@ from django.template import Template,Context
 from django.conf import settings
 import pyperclip
 import sys
-
+import os
 '''
 creating a python script that will read in the coverLetter template
 add the company name, and save it to a new file and the system's clipboard
 Also append the current date to the letters as well
 the compnay_name variable in the tempate is passed to the application
 '''
+CREATEDDIR = "createdCLetters/"
+TEMPLATEDIR = "templates/"
+COVERLETTER = "coverLetter.txt"
 
-#parses args passed to the application
+#creates a directory if one doesn't exist
+def createDir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        print "Directory "+dir+" was created."
+    else:
+	print "Directory "+dir+" already existed"
+
+#parses args passed to the application, checks template dir exist (otherwise application terminated),
+#creates the createdDir
+#TODO refactor out all the system verification code into its' own method
 def init():
+    print "Expected args: \"companyName>\""
     if sys.argv[0] == "createCoverLetter.py":
         argsList = sys.argv[1:]
     else:
         argsList = sys.argv
         if len(argsList) != 1:
-            print "Invalid number of arguments passed,"+str(len(argsList))+" , application terminating!"
-            print "Expected args: \"companyName>\""
+	    print "Invalid number of arguments passed,"+str(len(argsList))+" , application terminating!"        
             sys.exit()
-        else:
-            company_name = argsList[0]
-            print "company name received:"+company_name
+        company_name = argsList[0]
+        print "company name received:"+company_name
+    if not  os.path.exists(TEMPLATEDIR) :
+	print "There was not a template folder found in the indicated directory...application temrinating!"
+	sys.exit()
+    if not os.path.isfile(TEMPLATEDIR+COVERLETTER):
+	print "Issue with reading the cover letter..application terminating!"
+        sys.exit()
+    print "The cover letter at "+TEMPLATEDIR+COVERLETTER+" was successfully found"
+    createDir(CREATEDDIR)
 
 #Creates the cover letter and returns it as a String
 def createLetter():
-    fp=open("templates/coverLetter.txt","r")
+    fp=open(TEMPLATEDIR+COVERLETTER,"r")
     settings.configure()
     t = Template(fp.read())
     fp.close()
@@ -40,7 +60,7 @@ def createLetter():
 
 #writes the letter to the createdCLetters directory
 def writeLetter(letter):
-    file_name = "createdCLetters/"+sys.argv[1]+"CoverLetter.txt"
+    file_name = CREATEDDIR+sys.argv[1]+COVERLETTER
     outFile = open(file_name,"w+")
     outFile.write(letter)
     outFile.close()
@@ -50,6 +70,7 @@ def writeLetter(letter):
 def copyToClipboard(letter):
     pyperclip.setcb(letter)
     print "The cover letter has been appended to the system clipboard. Enjoy!"
+
 
 #start of application
 def main():
